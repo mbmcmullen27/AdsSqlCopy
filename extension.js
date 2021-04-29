7d979f3a20283526e6f3c67a580a4953e200ee02
fishbot <mbmcmullen27@gmail.com>
2021-04-29 15:43:09 -0500

    escape single quotes

--- extension.js
+++ extension.js
@@ -1,27 +1,29 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// The module 'azdata' contains the Azure Data Studio extensibility API
-// This is a complementary set of APIs that add SQL / Data-specific functionality to the app
-// Import the module and reference it with the alias azdata in your code below
-
 // Note: uncomment when you want to use Azure Data Studio APIs. Commenting now to avoid strict linting issues
-//const azdata = require('azdata');
+// const azdata = require('azdata');
 
 function activate(context) {
     context.subscriptions.push(vscode.commands.registerCommand('copyassqlvalues.copy', () => {
 			vscode.env.clipboard.readText()
 			.then((content)=>{
-				console.log(JSON.stringify(content))
+				//don't modify the clipboard if its already formatted as a list
 				if(!content.match(/\(('.*'),?\)/)){
 					vscode.window.showInformationMessage('making paste...');
+
+					//split by lines expecting windows-style line endings...
 					var tokens = content.split('\r\n')
 					tokens = tokens.filter((token)=>{return token!=null && token!=''})
-					vscode.env.clipboard.writeText('('+tokens.map(value=>'\''+value+'\'').join()+')')
+
+					vscode.env.clipboard.writeText(
+						//sql server escapes ' by doubling up '' replace them in each token
+						'('+tokens.map(value=>'\''+value.replace(/'/g,'\'\'')+'\'')
+						.join()+')'
+					)	
 					.then(()=>{
 						vscode.commands.executeCommand('editor.action.clipboardPasteAction')
 					})
+
 				}else{
 					vscode.window.showInformationMessage('eating paste...');
 					vscode.commands.executeCommand('editor.action.clipboardPasteAction')
@@ -29,7 +31,6 @@ function activate(context) {
 			})
 		})
     );
-
 }
 exports.activate = activate;
 
